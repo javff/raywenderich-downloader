9c936d07c87abbf9317c4d9c4f41ac1c736f6423
javff <juan.vasquez@mercadolibre.cl>
2021-08-16 18:33:58 -0400

    create new frontend

---
+++ RayDownloaderApp/RayDownloaderApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RayDownloaderApp
+//
+//  Created by Juan Andres Vasquez Ferrer on 16-08-21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
